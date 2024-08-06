495d7e7010c1362534c360c149a21ea47a64d0b0
Simon Willison <swillison@gmail.com>
2024-08-06 09:00:49 -0700

    Fixed bug introduced in 3b5b699461797fd61aa99dc20323e0bdaeb8b960

--- datasette_metadata_editable/internal_migrations.py
+++ datasette_metadata_editable/internal_migrations.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 internal_migrations = Migrations("datasette-metadata-editable.internal")
 
-SCHEMA = (Path(__file__).parent / "schema.sql").read_text()
-
 
 @internal_migrations()
 def m001_initialize_datasette_metadata_editable(db: Database):

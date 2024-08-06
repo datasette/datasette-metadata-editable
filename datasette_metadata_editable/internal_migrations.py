datasette/datasette-metadata-editable

commit 3b5b699461797fd61aa99dc20323e0bdaeb8b960

Remove schema.sql since that table is no longer used, refs #7

datasette_metadata_editable/internal_migrations.py
@@ -9,7 +9,24 @@ SCHEMA = (Path(__file__).parent / "schema.sql").read_text()
 
 @internal_migrations()
 def m001_initialize_datasette_metadata_editable(db: Database):
-    db.executescript(SCHEMA)
+    db.executescript(
+        """
+    create table datasette_metadata_editable_entries(
+        -- 'index' | 'database' | 'table' | 'column'
+        target_type text not null,
+        -- Uses empty string for "null" to enforce uniqueness
+        target_database text not null,
+        -- Uses empty string for "null" to enforce uniqueness
+        target_table text not null,
+        -- Uses empty string for "null" to enforce uniqueness
+        target_column text not null,
+        -- ex. 'description_html', 'source', 'license', 'about', etc.
+        key text not null,
+        value text,
+        UNIQUE(target_type, target_database, target_table, target_column, key)
+    );
+    """
+    )
 
 
 @internal_migrations()

datasette_metadata_editable/schema.sql
@@ -1,15 +1,0 @@
-create table datasette_metadata_editable_entries(
-  -- 'index' | 'database' | 'table' | 'column'
-  target_type text not null,
-  -- Uses empty string for "null" to enforce uniqueness
-  target_database text not null,
-  -- Uses empty string for "null" to enforce uniqueness
-  target_table text not null,
-  -- Uses empty string for "null" to enforce uniqueness
-  target_column text not null,
-  -- ex. 'description_html', 'source', 'license', 'about', etc.
-  key text not null,
-  value text,
-
-  UNIQUE(target_type, target_database, target_table, target_column, key)
-);

commit 495d7e7010c1362534c360c149a21ea47a64d0b0

Fixed bug introduced in 3b5b699461797fd61aa99dc20323e0bdaeb8b960

datasette_metadata_editable/internal_migrations.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 internal_migrations = Migrations("datasette-metadata-editable.internal")
 
-SCHEMA = (Path(__file__).parent / "schema.sql").read_text()
-
 
 @internal_migrations()
 def m001_initialize_datasette_metadata_editable(db: Database):