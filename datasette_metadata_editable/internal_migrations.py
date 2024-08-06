3b5b699461797fd61aa99dc20323e0bdaeb8b960
Simon Willison <swillison@gmail.com>
2024-08-05 21:43:29 -0700

    Remove schema.sql since that table is no longer used, refs #7

--- datasette_metadata_editable/internal_migrations.py
+++ datasette_metadata_editable/internal_migrations.py
@@ -9,7 +9,24 @@ SCHEMA = (Path(__file__).parent / "schema.sql").read_text()
 
 @internal_migrations()
 def m001_initialize_datasette_metadata_editable(db: Database):
-    db.executescript(SCHEMA)
+    db.executescript(
+        """
+    create table datasette_metadata_editable_entries(
+        -- 'index' | 'database' | 'table' | 'column'
+        target_type text not null,
+        -- Uses empty string for "null" to enforce uniqueness
+        target_database text not null,
+        -- Uses empty string for "null" to enforce uniqueness
+        target_table text not null,
+        -- Uses empty string for "null" to enforce uniqueness
+        target_column text not null,
+        -- ex. 'description_html', 'source', 'license', 'about', etc.
+        key text not null,
+        value text,
+        UNIQUE(target_type, target_database, target_table, target_column, key)
+    );
+    """
+    )
 
 
 @internal_migrations()
